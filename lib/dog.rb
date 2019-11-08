1a1451e0ac17869ac3de4361fe6e8cccaad63de3
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-08 20:21:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    @@all.each { |x| puts x }
+  
